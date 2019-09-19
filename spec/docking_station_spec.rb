37c96d6204f933d2e0283dad0e4b4fb35eb5086b
Heli Sivunen <heli@mooncat.tv>
2019-09-19 12:53:00 +0100

    Finishes exercise 12: 'no bikes available'if docking station is empty.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -8,15 +8,16 @@ describe DockingStation do
   # it { is_expected.to respond_to release_bike }
 
   describe '#release_bike' do
-    it "releases bike" do
-      bike = Bike.new
-      subject.dock(bike)
+    it "raises an error when there are no bikes available" do
+      # bike = Bike.new
+      # subject.dock(bike)
       #release docked bike
-      expect(subject.release_bike).to eq bike
+      expect { subject.release_bike }.to raise_error "No bikes available"
     end
   end
 
   it { is_expected.to respond_to(:dock).with(1).argument }
+
 
   it { is_expected.to respond_to(:bike) }
   # it "responds to bike" do
@@ -26,9 +27,5 @@ describe DockingStation do
     bike = Bike.new
     subject.dock(bike)
     expect(subject.bike).to eq bike
-  end
-
-  it "should return an error if empty dock" do
-
   end
 end
