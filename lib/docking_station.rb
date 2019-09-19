4811d28b3bb60f4384f78b7f8ead2b61395ca8cd
Heli Sivunen <heli@mooncat.tv>
2019-09-19 16:37:34 +0100

    Sets maximum number of bikes at docking station at 20.

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,17 +1,34 @@
 require_relative 'bike'
 
 class DockingStation
+  # attr_reader :bikes
+
+  def initialize
+    @bikes = []
+  end
+
   def release_bike
-    fail 'No bikes available' unless @bike
-    @bike
+    fail "No bikes available" if @bikes.empty?
+    @bikes.pop
+    # if @bikes.length < 1
+    #   fail 'No bikes available'
+    # else
+    #   @bikes.pop
+    # end
    end
 
   def dock(bike)
-   #add bike to self
-   @bike = bike
+       #add bike to self
+       fail "Docking station is full" if @bikes.count >= 20
+       @bikes << bike
+    # if @bikes.count >= 20
+    #   fail 'Docking station is full'
+    # else
+    #   @bikes << bike
+    # end
   end
 
-  def bike
-    @bike
+  def bikes
+    @bikes
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -16,16 +16,31 @@ describe DockingStation do
     end
   end
 
-  it { is_expected.to respond_to(:dock).with(1).argument }
+  describe '#dock' do
+    it { is_expected.to respond_to(:dock).with(1).argument }
 
+      it "raises an error when the dock is full" do
+        20.times { subject.dock(Bike.new) }
+        expect { subject.dock Bike.new }.to raise_error "Docking station is full"
+      end
+  #   instance = DockingStation.new.dock(Bike.new)
+  #
+#    expect(instance).to raise_error "Docking station is full"
+  end
 
-  it { is_expected.to respond_to(:bike) }
-  # it "responds to bike" do
-  #   instance = DockingStation.new
-  #   expect(instance).to respond_to(:bike)
-  it "returns docked bikes" do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.bike).to eq bike
+  describe 'bike' do
+    it { is_expected.to respond_to(:bikes) }
+    # it "responds to bike" do
+    #   instance = DockingStation.new
+    #   expect(instance).to respond_to(:bike)
+
+    it "tells you whether there are bikes at the docking station" do
+      # 1. given that there's a docking station
+      bike = Bike.new
+      # 2. when there is at least 1 bike
+      subject.dock(bike)
+      # 3. then it tells us whether there is at least 1 bike:
+      expect(subject.bikes).not_to be_empty
+    end
   end
 end
